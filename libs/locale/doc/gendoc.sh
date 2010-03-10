d977945a203082dcd30b24b755ef04106a005872
artyom-beilis <artyom-beilis@831565cc-8e21-4ad5-8334-98019c97bd36>
2010-03-10 22:37:46 +0000

    - Updated documentation generation - Added support of documentation installation
    git-svn-id: https://svn.code.sf.net/p/cppcms/code/boost_locale/trunk@1096 831565cc-8e21-4ad5-8334-98019c97bd36

--- libs/locale/doc/gendoc.sh
+++ libs/locale/doc/gendoc.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-if [ "$1" == "" ]; then
-	pandoc -T Boost.Locale -S --toc -c ../../../doc/html/boostbook.css -f markdown -t html index.txt -o index.html
-else
-	pandoc -T Boost.Locale -S --toc -c "$1"/doc/html/boostbook.css -f markdown -t html index.txt -o index.html
-fi
+rm -fr index.html html
+pandoc -B header.html -T Boost.Locale -S --toc -c boostbook.css -f markdown -t html index.txt -o index.html
+doxygen
+
 
