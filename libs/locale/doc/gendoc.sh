5546b6e582dc6b3041a59ea001659e328b1006f5
artyom-beilis <artyom-beilis@831565cc-8e21-4ad5-8334-98019c97bd36>
2010-03-11 21:21:49 +0000

    Tutorials are fully updated
    git-svn-id: https://svn.code.sf.net/p/cppcms/code/boost_locale/trunk@1099 831565cc-8e21-4ad5-8334-98019c97bd36

--- libs/locale/doc/gendoc.sh
+++ libs/locale/doc/gendoc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-rm -fr index.html html
-pandoc -B header.html -T Boost.Locale -S --toc -c boostbook.css -f markdown -t html index.txt -o index.html
+pandoc -B header.html -T Boost.Locale -S --toc -c boostbook.css -f markdown -t html index.txt -o tutorial.html
 doxygen
+cp tutorial.html boostbook.css boost_designed_for.png html
 
 
