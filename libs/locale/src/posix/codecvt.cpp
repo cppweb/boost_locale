f0af1d718476cfd0686f8ea5e57cf708547e2b79
artyom-beilis <artyom-beilis@831565cc-8e21-4ad5-8334-98019c97bd36>
2010-09-10 09:54:12 +0000

    Fixed build without iconv
    git-svn-id: https://svn.code.sf.net/p/cppcms/code/boost_locale/branches/rework@1423 831565cc-8e21-4ad5-8334-98019c97bd36

--- libs/locale/src/posix/codecvt.cpp
+++ libs/locale/src/posix/codecvt.cpp
@@ -224,7 +224,7 @@ namespace impl_posix {
     }
 
 #else // no iconv
-    std::auto_ptr<base_converter> create_iconv_converter(std::string const &/*encoding*/)
+    std::auto_ptr<util::base_converter> create_iconv_converter(std::string const &/*encoding*/)
     {
         std::auto_ptr<util::base_converter> cvt;
         return cvt;
