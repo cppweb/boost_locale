6a44ecbe332949cbddebe59cc1a50645dd689aa1
artyom-beilis <artyom-beilis@831565cc-8e21-4ad5-8334-98019c97bd36>
2011-03-24 12:36:25 +0000

    - Updated tutorial - Added autolink support
    git-svn-id: https://svn.code.sf.net/p/cppcms/code/boost_locale/trunk@1759 831565cc-8e21-4ad5-8334-98019c97bd36

--- boost/locale/config.hpp
+++ boost/locale/config.hpp
@@ -24,6 +24,28 @@
 #   define BOOST_LOCALE_DECL
 #endif
 
+//
+// Automatically link to the correct build variant where possible. 
+// 
+#if !defined(BOOST_ALL_NO_LIB) && !defined(BOOST_LOCALE_NO_LIB) && !defined(BOOST_LOCALE_SOURCE)
+//
+// Set the name of our library, this will get undef'ed by auto_link.hpp
+// once it's done with it:
+//
+#define BOOST_LIB_NAME boost_locale
+//
+// If we're importing code from a dll, then tell auto_link.hpp about it:
+//
+#if defined(BOOST_ALL_DYN_LINK) || defined(BOOST_LOCALE_DYN_LINK)
+#  define BOOST_DYN_LINK
+#endif
+//
+// And include the header that does the work:
+//
+#include <boost/config/auto_link.hpp>
+#endif  // auto-linking disabled
+
+
 
 #endif // boost/locale/config.hpp
 // vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
